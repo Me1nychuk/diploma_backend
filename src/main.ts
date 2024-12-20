09bfedff7b78737922398ba492afa2fb035210e3
Sviatoslav <svyatoslav200104@gmail.com>
2024-12-20 19:02:49 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000,https://diploma-backend-zvf0.onrender.com',
+    origin: [
+      'http://localhost:3000',
+      'https://diploma-backend-zvf0.onrender.com',
+    ],
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: ['Content-Type', 'Authorization'],
     exposedHeaders: ['Set-Cookie'],
